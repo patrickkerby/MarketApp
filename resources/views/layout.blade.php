c62b60ba74d8ab47c6a86eca9760f87d891a8e5e
Patrick Kerby <patrick@kerbydesign.com>
2020-10-17 08:44:43 -0600

    Add meta tags in head for no scaling & app capable

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,7 +2,8 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
   <head>
       <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0">
+      <meta name="apple-mobile-web-app-capable" content="yes" /> 
 
       <!-- CSRF Token -->
       <meta name="csrf-token" content="{{ csrf_token() }}">
