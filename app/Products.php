4bdcccb1c1e172d7825e58dac66e216d9747e469
Patrick Kerby <patrick@kerbydesign.com>
2024-06-20 09:47:46 -0600

    add soft delete to products

--- app/Products.php
+++ app/Products.php
@@ -4,9 +4,13 @@ namespace App;
 
 use App\categories;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class Products extends Model
 {
+    use SoftDeletes;
+    
     protected $guarded = [];
 
     public function path()
